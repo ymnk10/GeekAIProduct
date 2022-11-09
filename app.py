f9a5fb89d65771c500be5522d40a6aee012268c0
ymnk10 <ymnk10ymnk10@gmail.com>
2022-11-09 16:51:36 +0900

    h

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def delete():
 
 @app.route('/register_character', methods=['POST'])
 def register_character():
-    os.chmod("Tesseract-OCR",0o777)
+    os.chmod("Tesseract-OCR/tesseract.exe",0o777)
     # root = "https://kyousei-tyan.herokuapp.com/"
     # url = "https://kyousei-tyan.herokuapp.com/index2"
     # # store_path = "C:\\Users\\ymnk1\\GeekSalon\\OCR2\\pafumepic.png" #\は二つ！！
